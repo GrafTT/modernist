29eaf8620604c863edc8a6deb550c33f5db8ae24
Viacheslav Lamash <slava91billion@gmail.com>
2018-06-11 18:53:45 +0400

    Starting template with webpack

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+import $ from 'jquery';
+import popper from 'popper.js';
+import bootstrap from 'bootstrap';
+
+
+$(function() {
+    $('body').css('color', 'blue');
+});
+
+import '../scss/style.scss';
