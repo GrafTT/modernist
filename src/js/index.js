bc1d1e434bfa327d146f41b3c195e984e6f1603c
Viacheslav Lamash <slava91billion@gmail.com>
2018-07-03 00:46:38 +0400

    Form handler

--- src/js/index.js
+++ src/js/index.js
@@ -7,4 +7,27 @@ import dropdown from 'bootstrap/js/src/dropdown';
 
 
 
+const btn = document.querySelector('.contact__form_button');
+
+btn.addEventListener('click', (e)=>{
+  e.preventDefault();
+
+  fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'post',
+    body: JSON.stringify({
+      name: document.querySelector('[data-input=name]').value,
+      email: document.querySelector('[data-input=email]').value,
+      subject: document.querySelector('[data-input=subject]').value,
+      text: document.querySelector('textarea.contact__form_input').value
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8"
+    }
+  })
+  .then(res=>res.json())
+  .then(data=>alert(`Success subsribe ${data.name}`))
+})
+
+
+
 import '../scss/style.scss';
